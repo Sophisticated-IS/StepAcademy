64caec31297bd22667caeaae3c3a55c5d1c4beb8
Igor SI <test@ya.ru>
2023-03-26 22:45:01 +0500

    init

---
+++ StepAcademyApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace StepAcademyApp.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
