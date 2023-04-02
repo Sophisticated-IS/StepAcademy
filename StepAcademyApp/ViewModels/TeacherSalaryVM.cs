a1e7681243a4d806d23f0fc095056dc3040fb2da
Igor SI <test@ya.ru>
2023-04-02 23:30:24 +0500

    добавил TeacherSalaryUC.axaml

---
+++ StepAcademyApp/ViewModels/TeacherSalaryVM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace StepAcademyApp.ViewModels;
+
+internal sealed class TeacherSalaryVM
+{
+    internal sealed class MockSalary
+    {
+        public uint Год { get; set; }
+        public ushort Месяц { get; set; }
+        public decimal Зарплата { get; set; }
+    }
+
+    public ObservableCollection<MockSalary> РасчетЗарплаты { get; } = new();
+    public TeacherSalaryVM()
+    {
+        РасчетЗарплаты.Add(new MockSalary
+        {
+            Год = 2023,
+            Месяц = 1,
+            Зарплата = 10000
+        });
+        РасчетЗарплаты.Add(new MockSalary
+        {
+            Год = 2023,
+            Месяц = 2,
+            Зарплата = 20000
+            
+        });РасчетЗарплаты.Add(new MockSalary
+        {
+            Год = 2023,
+            Месяц = 3,
+            Зарплата = 30000
+        });
+    }
+}
---
+++ StepAcademyApp/Views/Application/MainWindow/TeacherSalaryUC.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StepAcademyApp.Views.Application;
+
+public partial class TeacherSalaryUC : UserControl
+{
+    public TeacherSalaryUC()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
