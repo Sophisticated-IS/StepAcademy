1093209ad8ac2b745f903a8091b69468cfa17157
Igor SI <test@ya.ru>
2023-06-07 11:43:42 +0500

    отображение зарплаты учителей

--- StepAcademyApp/ViewModels/TeacherSalaryVM.cs
+++ StepAcademyApp/ViewModels/TeacherSalaryVM.cs
@@ -1,37 +1,46 @@
-﻿using System.Collections.ObjectModel;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using StepAcademyApp.DataBase;
 
 namespace StepAcademyApp.ViewModels;
 
 internal sealed class TeacherSalaryVM
 {
-    internal sealed class MockSalary
+    internal sealed class TeacherSalary
     {
         public uint Год { get; set; }
         public ushort Месяц { get; set; }
         public decimal Зарплата { get; set; }
     }
 
-    public ObservableCollection<MockSalary> РасчетЗарплаты { get; } = new();
+    public ObservableCollection<TeacherSalary> РасчетЗарплаты { get; } = new();
     public TeacherSalaryVM()
     {
-        РасчетЗарплаты.Add(new MockSalary
-        {
-            Год = 2023,
-            Месяц = 1,
-            Зарплата = 10000
-        });
-        РасчетЗарплаты.Add(new MockSalary
-        {
-            Год = 2023,
-            Месяц = 2,
-            Зарплата = 20000
-            
-        });РасчетЗарплаты.Add(new MockSalary
-        {
-            Год = 2023,
-            Месяц = 3,
-            Зарплата = 30000
-        });
+        ReadSalary();
     }
+
+    private void ReadSalary()
+    {
+        try
+        {
+            using var dbContext = new StepAcademyDB(Program.DbContextOptions);
+            var salary = dbContext.Зарплаты.Where(x => x.IdУчитель == Program.CurrentUser.Id).ToList();
+            РасчетЗарплаты.Clear();
+            foreach (var sal in salary)
+            {
+                РасчетЗарплаты.Add(new TeacherSalary
+                {
+                    Год = sal.Year,
+                    Месяц = (ushort)sal.Month,
+                    Зарплата = sal.СуммаВыплат
+                });
+            }
+        }
+        catch (Exception exception)
+        {
+            Debug.WriteLine(exception);
+        }
+    } 
 }
