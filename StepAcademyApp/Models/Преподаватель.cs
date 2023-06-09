efd1682064e2e53be88744fcfa8b26835e0e5bd7
Igor SI <test@ya.ru>
2023-06-09 10:33:46 +0500

    поле админа

--- StepAcademyApp/Models/Преподаватель.cs
+++ StepAcademyApp/Models/Преподаватель.cs
@@ -5,5 +5,6 @@ namespace StepAcademyApp.Models;
 internal sealed class Преподаватель : Гражданин
 {
     public string НомерТелефона  { get; set; }
+    public bool Админ { get; set; }
     public TimeSpan Стаж { get; set; }
 }
