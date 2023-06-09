ac540fb5d2dfc2f041845a5770e06e270629d789
Igor SI <test@ya.ru>
2023-06-09 08:45:36 +0500

    fix student dt format

--- StepAcademyApp/ViewModels/StudentScheduleVM.cs
+++ StepAcademyApp/ViewModels/StudentScheduleVM.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using StepAcademyApp.DataBase;
 
 namespace StepAcademyApp.ViewModels;
 
-internal sealed class StudentScheduleVM
+internal sealed class StudentScheduleVM : ViewModelBase
 {
-    internal sealed class MockSchedule
+    internal sealed class StudSchedule
     {
         public string Начало { get; set; }
         public string Конец { get; set; }
@@ -13,41 +19,56 @@ internal sealed class StudentScheduleVM
         public string Предмет { get; set; }
     }
 
-    public ObservableCollection<MockSchedule> Расписание { get; } = new();
+    public ObservableCollection<StudSchedule> Расписание { get; } = new();
 
+    [Reactive]
+    public string CurrentDateTime { get; set; }
+
+    [Reactive]
+    public int PageNumberDay { get; set; }
     public StudentScheduleVM()
     {
-        Расписание.Add(
-            new MockSchedule()
+        this.WhenAnyValue(x => x.PageNumberDay).Subscribe(x =>
+        {
+            if (x<=0)
             {
-                Начало = "8:00",
-                Конец = "9:00",
-                ФИОПрепода = "Бурунов Олег Иванович",
-                ТипЗанятия = "Лекция",
-                Предмет = "Информатика"
+                PageNumberDay = 1;
             }
-        );
+            else
+            {
+                var dt = DateTime.Now;
+                CurrentDateTime = dt.AddDays(-dt.Day).AddDays(PageNumberDay).ToString("dddd, dd MMMM, yyyy");
+                using var dbContext = new StepAcademyDB(Program.DbContextOptions);
+                var student = dbContext.Студенты.First(x=>x.Id == Program.CurrentUser.Id);
+                var schedule = dbContext.Нагрузка
+                                        .Where(x => x.IdГруппы == student.IdГруппы
+                                        && x.ВремяНачалаЗанятия.Date.Day == PageNumberDay
+                                        && x.ВремяКонцаЗанятия.Date.Month == DateTime.Now.Month
+                                        && x.ВремяКонцаЗанятия.Date.Year == DateTime.Now.Year)
+                                        .Include(x=>x.Преподаватель)
+                                        .Include(x=>x.ТипЗанятия)
+                                        .Include(x=>x.Предмет)
+                                        .ToList();
+                Расписание.Clear();
+                foreach (var item in schedule)
+                {
+                    Расписание.Add(new StudSchedule
+                    {
+                        Начало = item.ВремяНачалаЗанятия.TimeOfDay.ToString(),
+                        Конец = item.ВремяКонцаЗанятия.TimeOfDay.ToString(),
+                        ФИОПрепода = item.Преподаватель.Имя + " " + item.Преподаватель.Фамилия + " " + item.Преподаватель.Отчество,
+                        ТипЗанятия = item.ТипЗанятия.Название,
+                        Предмет = item.Предмет.Название
+                        
+                    });
+                }
+
+            }
+            
+        });
         
-        Расписание.Add(
-            new MockSchedule()
-            {
-                Начало = "9:00",
-                Конец = "10:00",
-                ФИОПрепода = "Бурунов Олег Иванович",
-                ТипЗанятия = "Лабораторная",
-                Предмет = "Информатика"
-            }
-        );
-        
-        Расписание.Add(
-            new MockSchedule()
-            {
-                Начало = "10:30",
-                Конец = "12:00",
-                ФИОПрепода = "Гаврилов Михаил Анатольевич",
-                ТипЗанятия = "Лекция",
-                Предмет = "История"
-            }
-        );
+        PageNumberDay = 1;
+
     }
+    
 }
