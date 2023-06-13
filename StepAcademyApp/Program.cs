0827ee2f576f04b1ef0481e7aa348e699f7fa205
Brusnitsin, Maksim P <bmp1999@inbox.ru>
2023-06-13 13:05:47 +0500

    Update Program.cs

--- StepAcademyApp/Program.cs
+++ StepAcademyApp/Program.cs
@@ -228,7 +228,7 @@ namespace StepAcademyApp
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=StepAcademyDB;Username=postgres;Password=postgres");
             var dbContext = new StepAcademyDB(optionsBuilder.Options);
             DbContextOptions = optionsBuilder.Options;
-            dbContext.Database.EnsureDeleted();
+            //dbContext.Database.EnsureDeleted();
             FillDbCsv(dbContext);
             if (dbContext.Database.EnsureCreated())
             {
